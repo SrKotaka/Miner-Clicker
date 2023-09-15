f35ab896f49b109f72498fd4af8b31e751e30ff8
FELIPE LUVIZOTTO DE CASTRO <u22129@academico.cotuca.unicamp.br>
2023-09-15 09:15:45 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -14,15 +16,18 @@ class Activity_register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
-        val buttonRegister: Button = findViewById(R.id.buttonRegister)
-        buttonRegister.setOnClickListener {
-
-        }
     }
 
     fun goToLogin(view: View) {
         val login = Intent(this@Activity_register, Activity_login::class.java)
         startActivity(login)
 
+    }
+
+    fun registerInDataBase(view: View) {
+        val database = Firebase.database
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello world")
     }
 }
