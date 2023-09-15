6e0729da0ce0a494afb3c016e484181b3e004e23
FELIPE LUVIZOTTO DE CASTRO <u22129@academico.cotuca.unicamp.br>
2023-09-15 09:37:40 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -6,6 +6,7 @@ import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
 import retrofit2.Call
@@ -27,7 +28,11 @@ class Activity_register : AppCompatActivity() {
     fun registerInDataBase(view: View) {
         val database = Firebase.database
         val myRef = database.getReference("User")
-        val nome = findViewById.(R.id.editTextUsername)
-        myRef.setValue(nome,email,senha)
+        val nome = findViewById<EditText>(R.id.editTextUsername).text.toString()
+        val email = findViewById<EditText>(R.id.editTextEmail).text.toString()
+        val senha = findViewById<EditText>(R.id.editTextPassword).text.toString()
+        myRef.setValue("${"nome: " + nome + " \r\n email: " + email + " \r\n senha: " + senha}")
     }
 }
+
+
