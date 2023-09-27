6f8031007a158a36b611e728c26a0f91a52bc950
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-09-27 18:35:54 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_login.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_login.kt
@@ -4,6 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.FirebaseApp
+import android.widget.Toast
 
 
 class Activity_login : AppCompatActivity() {
@@ -13,8 +17,49 @@ class Activity_login : AppCompatActivity() {
     }
 
     fun goToRegister(view: View) {
-        val register = Intent(this@Activity_login, Activity_register::class.java)
-        startActivity(register)
+        try {
+            val register = Intent(this@Activity_login, Activity_register::class.java)
+            startActivity(register)
+        }
+        catch (e: Exception){
+            Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
+
+    fun loginInDataBase(view: View) {
+        val email = findViewById<EditText>(R.id.editTextLoginEmail).text.toString()
+        val senha = findViewById<EditText>(R.id.editTextLoginPassword).text.toString()
+
+        val db = FirebaseFirestore.getInstance()
+        val usersCollection = db.collection("usuarios")
+
+        // Query the Firestore collection to find a user with the provided email and password
+        usersCollection.whereEqualTo("email", email)
+            .whereEqualTo("senha", senha)
+            .get()
+            .addOnSuccessListener { querySnapshot ->
+                if (!querySnapshot.isEmpty) {
+                    // Login successful, user with matching email and password found
+                    val game = Intent(this@Activity_login, MainActivity::class.java)
+                    startActivity(game)
+                } else {
+                    // No user found with the provided credentials
+                    Toast.makeText(
+                        applicationContext,
+                        "Login failed. Please check your email and password.",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+            .addOnFailureListener { e ->
+                // Handle any errors that occur during the query
+                Toast.makeText(
+                    applicationContext,
+                    "Login failed. Please try again.",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
 
 }
--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -5,17 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.FirebaseApp
+import android.widget.Toast
 
 class Activity_register : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        FirebaseApp.initializeApp(this)
     }
 
     fun goToLogin(view: View) {
-        val login = Intent(this@Activity_register, Activity_login::class.java)
-        startActivity(login)
+        try {
+            val login = Intent(this@Activity_register, Activity_login::class.java)
+            startActivity(login)
+        } catch (e: Exception) {
+            Toast.makeText(applicationContext, "${e.message}", Toast.LENGTH_SHORT).show()
+        }
     }
 
     fun registerInDataBase(view: View) {
@@ -23,11 +32,24 @@ class Activity_register : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.editTextRegisterEmail).text.toString()
         val senha = findViewById<EditText>(R.id.editTextRegisterPassword).text.toString()
 
+        val db = FirebaseFirestore.getInstance()
+
+        // Create a collection reference and add a new document
+        val usersCollection = db.collection("usuarios")
         val userData = hashMapOf(
-            "name" to nome,
             "email" to email,
+            "nome" to nome,
             "senha" to senha
         )
+
+        usersCollection.add(userData)
+            .addOnSuccessListener { documentReference ->
+                val game = Intent(this@Activity_register, MainActivity::class.java)
+                startActivity(game)
+            }
+            .addOnFailureListener { e ->
+                Toast.makeText(applicationContext, "Seu registro não funcionou, por favor tente novamente", Toast.LENGTH_SHORT).show()
+            }
 
     }
 
