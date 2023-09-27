94cd6e4bd6f40887d12ab08fc7829a0f486b617b
FELIPE LUVIZOTTO DE CASTRO <u22129@academico.cotuca.unicamp.br>
2023-09-27 08:54:32 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -6,12 +6,18 @@ import android.view.View
 import android.widget.EditText
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
-
+import com.google.firebase.firestore.FirebaseFirestore
 
 class Activity_register : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var firestore: FirebaseFirestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        auth = FirebaseAuth.getInstance()
+        firestore = FirebaseFirestore.getInstance()
     }
 
     fun goToLogin(view: View) {
@@ -25,7 +31,23 @@ class Activity_register : AppCompatActivity() {
         val email = findViewById<EditText>(R.id.editTextRegisterEmail).text.toString()
         val senha = findViewById<EditText>(R.id.editTextRegisterPassword).text.toString()
 
-        
+        val userData = hashMapOf(
+            "name" to nome,
+            "email" to email,
+            "senha" to senha
+        )
+        firestore.collection("usuarios")
+            .add(userData)
+            .addOnSuccessListener { documentReference ->
+                // Document added successfully
+                Toast.makeText(this@Activity_register, "User registered successfully", Toast.LENGTH_SHORT).show()
+                val game = Intent(this@Activity_register, MainActivity::class.java)
+                startActivity(game)
+            }
+            .addOnFailureListener { e ->
+                // Handle errors
+                Toast.makeText(this@Activity_register, "Error: ${e.message}", Toast.LENGTH_SHORT).show()
+            }
 
         //FirebaseAuth.getInstance().createUserWithEmailAndPassword(nome,email, senha)
         //    .addOnCompleteListener { task ->
