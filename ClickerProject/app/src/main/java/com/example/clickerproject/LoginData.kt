020eea1f83737a6a36075fe40844acc3baddb058
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-10-12 12:33:10 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/LoginData.kt
+++
@@ -1,6 +0,0 @@
-package com.example.clickerproject
-
-data class LoginData(
-    val email: String,
-    val senha: String
-)
--- ClickerProject/app/src/main/java/com/example/clickerproject/RegisterData.kt
+++
@@ -1,7 +0,0 @@
-package com.example.clickerproject
-
-data class RegisterData(
-    val email: String,
-    val nome: String,
-    val senha: String
-)
