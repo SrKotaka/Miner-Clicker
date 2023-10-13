05b6240702a8c75201d17f44c60764d4540ee370
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-10-13 11:46:27 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -7,6 +7,11 @@ import android.view.View
 import android.widget.TextView
 import android.widget.Toast
 //daqui pra baixo eu que coloquei
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 
 class Activity_register : AppCompatActivity() {
@@ -17,7 +22,38 @@ class Activity_register : AppCompatActivity() {
     }
 
     fun registerInDataBase(view: View) {
-        
+        val email = findViewById<TextView>(R.id.editTextEmail).text.toString() // Replace with your EditText IDs
+        val name = findViewById<TextView>(R.id.editTextName).text.toString() // Replace with your EditText IDs
+        val password = findViewById<TextView>(R.id.editTextPassword).text.toString() // Replace with your EditText IDs
+
+        // Check if any field is empty
+        if (email.isEmpty() || name.isEmpty() || password.isEmpty()) {
+            Toast.makeText(applicationContext, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val request = JSONObject()
+        request.put("email", email)
+        request.put("name", name)
+        request.put("password", password)
+
+        val queue: RequestQueue = Volley.newRequestQueue(this)
+        val url = "http://localhost:3000/usuarios" // Replace with your API URL
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.POST, url, request,
+            { response ->
+                // Registration successful, redirect to the login page
+                val goToGame = Intent(this, MainActivity::class.java)
+                startActivity(goToGame)
+            },
+            { error ->
+                // Registration failed
+                Toast.makeText(applicationContext, "Registration failed: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+
+        queue.add(jsonObjectRequest)
     }
 
     fun goToLogin(view: View) {
