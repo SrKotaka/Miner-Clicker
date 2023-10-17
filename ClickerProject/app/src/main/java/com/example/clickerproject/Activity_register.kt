63e3e88f9488f2150390db9200fc20d53cf7d4e4
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-10-17 20:23:55 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_register.kt
@@ -9,7 +9,6 @@ import android.widget.Toast
 //From here down I put it
 import com.android.volley.Request
 import com.android.volley.RequestQueue
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
 import org.json.JSONObject
