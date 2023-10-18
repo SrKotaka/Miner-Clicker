bf3b537ce0f1c57c3ca81ff33641febd06bb056c
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-10-17 21:38:50 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_login.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_login.kt
@@ -4,7 +4,13 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class Activity_login : AppCompatActivity() {
 
@@ -14,7 +20,33 @@ class Activity_login : AppCompatActivity() {
     }
 
     fun loginInDataBase(view: View) {
+        val email = findViewById<TextView>(R.id.editTextEmail).text.toString()
+        val password = findViewById<TextView>(R.id.editTextPassword).text.toString()
 
+        if (email.isEmpty() || password.isEmpty()) {
+            Toast.makeText(applicationContext, "Please fill in all fields", Toast.LENGTH_SHORT).show()
+            return
+        }
+
+        val queue: RequestQueue = Volley.newRequestQueue(this)
+        val url = "http://192.168.130.80:3000/usuarios/$email"
+
+        val jsonObjectRequest = JsonObjectRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                if(password == response.getString("password"))
+                {
+                    val goToGame = Intent(this, MainActivity::class.java)
+                    startActivity(goToGame)
+                } else {
+                    Toast.makeText(applicationContext, "Wrong password", Toast.LENGTH_SHORT).show()
+                }
+            },
+            { error ->
+                Toast.makeText(applicationContext, "Invalid email", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
     }
 
     fun goToRegister(view: View) {
--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_rank.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_rank.kt
@@ -3,13 +3,45 @@ package com.example.clickerproject
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+//From here down I put it
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import org.json.JSONObject
 
 class Activity_rank : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rank)
+
+        val rankTextView = findViewById<TextView>(R.id.rank)
+        val queue: RequestQueue = Volley.newRequestQueue(this)
+        val url = "http://192.168.130.80:3000/usuarios"
+
+        val jsonObjectRequest = JsonArrayRequest(
+            Request.Method.GET, url, null,
+            { response ->
+                try {
+                    val jsonArray = response.getJSONArray(0)
+                    for (i in 0 until jsonArray.length()) {
+                        val jsonObject = jsonArray.getJSONArray(i).getJSONObject(0)
+                        val rank = jsonObject.getString("rank")
+                        rankTextView.text = rank
+                    }
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            },
+            { error ->
+                Toast.makeText(applicationContext, "Error to pull database: ${error.message}", Toast.LENGTH_SHORT).show()
+            }
+        )
+        queue.add(jsonObjectRequest)
     }
 
     fun goToMain(view: View) {
