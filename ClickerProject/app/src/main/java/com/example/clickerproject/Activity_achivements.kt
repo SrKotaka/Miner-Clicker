db10c77c1a0a3b43756ec5a278f38e3177baf43a
Kotaka <101439293+SrKotaka@users.noreply.github.com>
2023-10-17 22:50:32 -0300

    git

--- ClickerProject/app/src/main/java/com/example/clickerproject/Activity_achivements.kt
+++ ClickerProject/app/src/main/java/com/example/clickerproject/Activity_achivements.kt
@@ -1,8 +1,10 @@
 package com.example.clickerproject
 
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
@@ -10,6 +12,13 @@ class Activity_achivements : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_achivements)
+
+        //val achivement1 = findViewById<ImageView>(R.id.achivement1)
+        //if (coins >= 1000) {
+        //    achivement1.setBackgroundColor(Color.WHITE)
+        //} else {
+        //    achivement1.setBackgroundColor(Color.GRAY)
+        //}
     }
 
     fun goToMain(view: View) {
